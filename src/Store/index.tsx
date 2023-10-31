9eb479b243b1393264138232069ed265913d7125
Fermin Blanco <luillyfe89@gmail.com>
2023-10-31 10:02:42 -0500

    refactor: adding dispatch function to Store context

--- src/Store/index.tsx
+++ src/Store/index.tsx
@@ -1,17 +1,22 @@
-import { ReactNode, createContext, useReducer } from "react";
+import { ReactNode, createContext, useReducer, Dispatch } from "react";
 
-import reducerStore, { Store } from "./ReducerStore";
+import reducerStore, { Action, Store } from "./ReducerStore";
 
+// TODO: Must be an object not an array
 const store: Store = {
   todos: [],
 };
 
-export const StoreContext = createContext(store);
+const dispatch: Dispatch<Action> = () => null;
+
+export const StoreContext = createContext({ store, dispatch });
 
 export function StoreProvider({ children }: { children: ReactNode }) {
-  const [state] = useReducer(reducerStore, store);
+  const [state, dispatch] = useReducer(reducerStore, store);
 
   return (
-    <StoreContext.Provider value={state}>{children}</StoreContext.Provider>
+    <StoreContext.Provider value={{ store: state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
   );
 }
