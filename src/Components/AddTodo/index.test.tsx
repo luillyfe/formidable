b41d0178299871270d13558f7b02325b6d0ebcc1
Fermin Blanco <luillyfe89@gmail.com>
2023-11-01 14:18:19 -0500

    fix: test case

--- src/Components/AddTodo/index.test.tsx
+++ src/Components/AddTodo/index.test.tsx
@@ -10,8 +10,12 @@ test("Must render the component for adding a todo", () => {
   render(<AddTodo />, { wrapper: BrowserRouter });
 
   // Assert
-  // must render a form
+  // Must render the form for adding new todos
   expect(screen.getByRole("form")).toBeInTheDocument();
+  expect(screen.getByRole("textbox", { name: "Title" })).toBeInTheDocument();
+  expect(
+    screen.getByRole("textbox", { name: "Todo description" })
+  ).toBeInTheDocument();
   // two buttons must be on the screen
   expect(screen.getAllByRole("button")).toHaveLength(2);
   // Back button must on the screen
