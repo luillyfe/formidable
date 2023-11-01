bb9ae8e890c6f95abc5dba120509ea0ddbd74b51
Fermin Blanco <luillyfe89@gmail.com>
2023-11-01 10:54:26 -0500

    chore: testing rendering 5 todos

--- src/Components/ListTodos/index.test.tsx
+++ src/Components/ListTodos/index.test.tsx
@@ -1,10 +1,33 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 import ListTodos from "./index";
 
 test("A new button must be on component", () => {
   // Arrange
-  render(<ListTodos todos={[{ id: 1, title: "", description: "" }]} />);
+  render(<ListTodos todos={[]} />, {
+    wrapper: BrowserRouter,
+  });
 
   // Assert
-  expect(screen.queryAllByRole("link")).toHaveLength(1);
+  expect(screen.getByText(/New/i)).toBeInTheDocument();
+});
+
+describe("Rendering todos", () => {
+  const todos = [
+    { id: 1, title: "todo1", description: "description1" },
+    { id: 2, title: "todo2", description: "description2" },
+    { id: 3, title: "todo3", description: "description3" },
+    { id: 4, title: "todo4", description: "description4" },
+    { id: 5, title: "todo5", description: "description5" },
+  ];
+  test("Must render 5 links", () => {
+    // Arrange
+    render(<ListTodos todos={todos} />, {
+      wrapper: BrowserRouter,
+    });
+
+    // Assert
+    expect(screen.queryAllByRole("link")).toHaveLength(5);
+  });
 });
