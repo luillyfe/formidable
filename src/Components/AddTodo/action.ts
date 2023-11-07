2c34a41ccfcbadf6da360ae0d23604e75759fb9e
Fermin Blanco <luillyfe89@gmail.com>
2023-11-07 16:31:38 -0500

    refactor: generating uuid from library

--- src/Components/AddTodo/action.ts
+++ src/Components/AddTodo/action.ts
@@ -1,14 +1,17 @@
 import { redirect } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { addTodo } from "../../Store/actions";
 
 export async function handleTodoSubmit({ request }: { request: Request }) {
   const formData = await request.formData();
   const entries = Object.fromEntries(formData);
   const todo = {
-    id: Math.random() * 10000,
+    id: uuidv4(),
     title: String(entries.title),
     description: String(entries.description),
   };
 
-  console.log(todo);
+  await addTodo(todo);
+
   return redirect("/");
 }
