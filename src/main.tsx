7ca8fc95f4caf86b521111c0daf25fd3be850383
Fermin Blanco <luillyfe89@gmail.com>
2023-11-08 17:40:40 -0500

    chore: adding QueryProvider and Query client to React tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 import { AppWithRouter } from "./Routes/index";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppWithRouter />
+    <QueryClientProvider client={queryClient}>
+      <AppWithRouter />
+    </QueryClientProvider>
   </React.StrictMode>
 );
