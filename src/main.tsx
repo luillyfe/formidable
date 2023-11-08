ae2609f3759e871435de63a1fa5eb5ffacdd48db
Fermin Blanco <luillyfe89@gmail.com>
2023-11-08 15:54:54 -0500

    refactor: removing store provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import ReactDOM from "react-dom/client";
 import { AppWithRouter } from "./Routes/index";
 
 import "./index.css";
-import { StoreProvider } from "./Store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <StoreProvider>
-      <AppWithRouter />
-    </StoreProvider>
+    <AppWithRouter />
   </React.StrictMode>
 );
