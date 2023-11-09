e28b199ea5d76654fc5cd994e889ab9abec741fb
Fermin Blanco <luillyfe89@gmail.com>
2023-11-09 15:30:19 -0500

    refactor: adding query provider to test cases

--- src/Components/ListTodos/index.tsx
+++ src/Components/ListTodos/index.tsx
@@ -36,7 +36,7 @@ export default function ListTodos() {
     <div className="relative">
       <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
         <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-          <div className="p-4">
+          <div className="p-4" role="list">
             {todos.map((todo) => (
               <Todo
                 title={todo.title}
--- src/Components/Todo/index.tsx
+++ src/Components/Todo/index.tsx
@@ -2,7 +2,10 @@ import { Task } from "../../Store/types";
 
 export default function Todo({ title, description }: Task) {
   return (
-    <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+    <div
+      className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50"
+      role="document"
+    >
       <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
         <svg
           className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
