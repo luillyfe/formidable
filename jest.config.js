e656245191041c231430c129af985b03159ef9fb
Fermin Blanco <luillyfe89@gmail.com>
2023-12-10 13:55:23 -0500

    chores: setting support for svg in jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const config = {
   verbose: true,
   testEnvironment: "jsdom",
   transformIgnorePatterns: ["node_modules/(?!(@web3-storage)/)"],
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/__mocks__/svg.js",
+  },
 };
 
 export default config;
